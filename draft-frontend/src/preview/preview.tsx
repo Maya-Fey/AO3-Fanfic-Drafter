6a1ad2da8ea05e57cc9992833ca33a2a898307c4
Claire Fey <foxcoindev@gmail.com>
2022-02-20 04:59:01 +1300

    ADF-14 & ADF-15: Show compiled source in output window

--- draft-frontend/src/preview/preview.tsx
+++ draft-frontend/src/preview/preview.tsx
@@ -5,32 +5,42 @@ import { TabbedWindow } from "../tabs/TabbedWindow";
 import { OutputDisplayTab } from "./outputdisplay";
 import { PreviewDisplayTab } from "./previewdisplay";
 
+import { observer } from "mobx-react";
+import { compileFanfic, FicCompilerError } from "../compiler/compiler";
+import { EditorTarget } from "../fanfic/editortarget";
+
 enum PreviewTab {
     PREVIEW = "Preview",
     OUTPUT = "Output",
 }
 
 export class PreviewContext {
-    tabCtx: TabbedContext<PreviewProps, PreviewTab>;
+    tabCtx: TabbedContext<PreviewTabProps, PreviewTab>;
 
     constructor() {
         makeAutoObservable(this);
-        let tabs: Map<PreviewTab, Tab<PreviewProps>> = new Map<PreviewTab, Tab<PreviewProps>>();
+        let tabs: Map<PreviewTab, Tab<PreviewTabProps>> = new Map<PreviewTab, Tab<PreviewTabProps>>();
         tabs.set(PreviewTab.PREVIEW, new PreviewDisplayTab());
         tabs.set(PreviewTab.OUTPUT, new OutputDisplayTab());
-        this.tabCtx = new TabbedContext<PreviewProps, PreviewTab>(PreviewTab.PREVIEW, tabs);
+        this.tabCtx = new TabbedContext<PreviewTabProps, PreviewTab>(PreviewTab.PREVIEW, tabs);
     }
 }
 
-export interface PreviewProps extends React.HTMLProps<HTMLDivElement> {
+interface PreviewProps extends React.HTMLProps<HTMLDivElement> {
     ctx: PreviewContext;
+    targ: EditorTarget;
     fic: FanficContext;
 }
 
-export function Preview(props: PreviewProps) {
+export interface PreviewTabProps extends PreviewProps {
+    compiled: string[]|FicCompilerError
+}
+
+export const Preview = observer(function(props: PreviewProps) {
+    let ret: string[]|FicCompilerError = compileFanfic(props.fic.fic, props.targ);
     return (
         <div className="preview">
-            <TabbedWindow<PreviewProps, PreviewTab> ctx={props.ctx.tabCtx} props={props}/>
+            <TabbedWindow<PreviewTabProps, PreviewTab> ctx={props.ctx.tabCtx} props={{ ctx: props.ctx, targ: props.targ, fic: props.fic, compiled: ret }}/>
         </div>
     );
-}
+});
