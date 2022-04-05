68535a3203794f2dacb643818afc6a249d65d478
Claire Fey <foxcoindev@gmail.com>
2022-04-05 01:11:03 -0700

    AFB-08: Add new fanfic servlet

---
+++ draft-backend/src/main/java/host/claire/ao3fanficdrafter/backend/servlet/framework/ContentOutgoingPacket.java
@@ -0,0 +1,35 @@
+package host.claire.ao3fanficdrafter.backend.servlet.framework;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import host.claire.ao3fanficdrafter.backend.servlet.framework.virtual.OutgoingPacket;
+
+public class ContentOutgoingPacket implements OutgoingPacket {
+	
+	protected final Gson gson = new GsonBuilder().create();
+	
+	protected final Object o;
+	
+	public ContentOutgoingPacket(Object o) {
+		this.o = o;
+	}
+
+	@Override
+	public void populate(HttpServletResponse resp) {
+		try {
+			resp.getWriter().write(gson.toJson(this.o));
+		} catch (IOException e) {
+			e.printStackTrace();
+			resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			return;
+		}
+		resp.setContentType("application/json");
+		resp.setStatus(HttpServletResponse.SC_OK);
+	}
+
+}
--- draft-backend/src/main/java/host/claire/ao3fanficdrafter/backend/servlet/framework/StatusOnlyOutgoingPacket.java
+++ draft-backend/src/main/java/host/claire/ao3fanficdrafter/backend/servlet/framework/StatusOnlyOutgoingPacket.java
@@ -4,11 +4,18 @@ import javax.servlet.http.HttpServletResponse;
 
 import host.claire.ao3fanficdrafter.backend.servlet.framework.virtual.OutgoingPacket;
 
-public abstract class AbstractOutgoingPacket implements OutgoingPacket {
+public class StatusOnlyOutgoingPacket implements OutgoingPacket {
+	
+	private final int status;
+
+	public StatusOnlyOutgoingPacket(int status) {
+		super();
+		this.status = status;
+	}
 
 	@Override
 	public void populate(HttpServletResponse resp) {
-		
+		resp.setStatus(this.status);
 	}
 
 }
