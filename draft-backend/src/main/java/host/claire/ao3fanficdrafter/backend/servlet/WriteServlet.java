e7465b81052638feec12bd5e3898290173d82d73
Claire Fey <foxcoindev@gmail.com>
2022-04-05 20:02:40 -0700

    AFB-09: Add write servlet

---
+++ draft-backend/src/main/java/host/claire/ao3fanficdrafter/backend/servlet/WriteServlet.java
@@ -0,0 +1,28 @@
+package host.claire.ao3fanficdrafter.backend.servlet;
+
+import javax.servlet.ServletConfig;
+
+import host.claire.ao3fanficdrafter.backend.servlet.framework.InnerServletAuthenticationAdapter;
+import host.claire.ao3fanficdrafter.backend.servlet.framework.OuterServlet;
+import host.claire.ao3fanficdrafter.backend.servlet.framework.ServletGlobalState;
+import host.claire.ao3fanficdrafter.backend.servlet.framework.virtual.IncomingPacket;
+import host.claire.ao3fanficdrafter.backend.servlet.framework.virtual.InnerServlet;
+
+public class WriteServlet extends OuterServlet {
+
+	private static final long serialVersionUID = 5928551237085149299L;
+
+	private InnerServlet<IncomingPacket> serv;
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	public void init(ServletConfig conf) {
+		this.serv = (InnerServlet<IncomingPacket>) new InnerServletAuthenticationAdapter(ServletGlobalState.instance.getVerify(), new WriteFicInnerServlet(ServletGlobalState.instance.getStorage()));
+	}
+	
+	@Override
+	protected InnerServlet<IncomingPacket> getInner() {
+		return this.serv;
+	}
+
+}
