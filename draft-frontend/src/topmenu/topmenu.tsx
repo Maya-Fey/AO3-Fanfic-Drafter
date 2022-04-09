c6fedce741006757f0a87538519fe2123fc4cf97
Claire Fey <foxcoindev@gmail.com>
2022-04-09 14:57:30 -0700

    ADF-53: Add Fic Select

--- draft-frontend/src/topmenu/topmenu.tsx
+++ draft-frontend/src/topmenu/topmenu.tsx
@@ -1,4 +1,5 @@
 import { AppContext } from "../App";
+import { FicSwitcher } from "./ficswitcher";
 import { FocusSwitcher } from "./foccusswitcher";
 import { ServerConnect } from "./serverconnect";
 
@@ -10,6 +11,7 @@ export function TopMenu(props: TopMenuProps) {
     return (
         <div className="top-menu">
             <FocusSwitcher ctx={props.ctx}/>
+            <FicSwitcher ctx={props.ctx}/>
             <hr/>
             <ServerConnect ctx={props.ctx}/>
         </div>
